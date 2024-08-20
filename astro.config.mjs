180a5a53f5a78ebb5bf1d13f8f757c1c5978d3a5
daar <dhkblaszyk@gmail.com>
2024-08-20 14:08:37 +0200

    implement basic blogging and pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,12 @@
 import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://daar.github.io/',
+	integrations: [mdx(), sitemap()],
     i18n: {
         defaultLocale: "nl",
         locales: ["es", "en", "nl"],
