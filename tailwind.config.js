8c027f06411509ea6b5f4e97212cf27d7f9430b2
Darius Blaszyk <dhkblaszyk@gmail.com>
2024-08-28 11:59:10 +0200

    feat(app): Add tailwind to project
    Add tailwind support + test features with a blogroll and blogpost page

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{astro,html,js,jsx,ts,tsx,vue}", // Add your paths here
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
