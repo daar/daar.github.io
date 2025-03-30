b3819a75d9dc4ef33f2cb64765d411ba4f061e17
Darius Blaszyk <dhkblaszyk@gmail.com>
2025-03-30 21:45:09 +0200

    change code, based on the astro-photo-gallery project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,4 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
-import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
-export default defineConfig({
-    site: 'https://daar.github.io/',
-    vite: {
-      plugins: [tailwindcss()],
-    },
-  });
+export default defineConfig({})
