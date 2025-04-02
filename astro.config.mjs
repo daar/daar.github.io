9494a6973daa30d1a383d6c5a8b483ecf9cc3cc4
Darius Blaszyk <dhkblaszyk@gmail.com>
2025-04-02 19:48:21 +0200

    A new turn again! Previous code was becomming too complex for what I need now. KISS

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
-import { defineConfig } from 'astro/config'
+// @ts-check
+import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-    vite: {
-      plugins: [tailwindcss()],
-    },
-  });
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
